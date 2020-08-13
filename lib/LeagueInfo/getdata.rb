d791828ca572781faba548531a4b37c6228d125b
Cristian Deleon <libaration@gmail.com>
2020-08-13 02:07:23 -0400

    Fixed scrape_kda raising error when match history wasn't updated. Updated to more reliable/updated website for KDA's

--- lib/LeagueInfo/getdata.rb
+++ lib/LeagueInfo/getdata.rb
@@ -27,14 +27,17 @@ class LeagueInfo::Getdata
     usersCollected.sample
   end
 
+
   def self.scrape_kda(name)
     name = URI.escape name
-    url = "https://lolprofile.net/summoner/na/#{name}"
+    kdaArray = Array.new
+    url = "https://na.op.gg/summoner/userName=#{name}"
     doc = Nokogiri::HTML(open(url))
-    rows = doc.css("div.s-rg")
-    kda = rows.css("span.kda.victory-text1")
-    kdaArray = Array.new.tap do |array|
-      kda.each{|thing| array << thing.text.gsub(/\s+/, "").split("/")}
+    rows = doc.css("div.GameItemWrap").each do |row|
+      kdaArray << [row.css("span.Kill").text.gsub(/[a-z\s]|[A-Z\s]/, ""), row.css("span.Death").text.gsub(/[a-z\s]|[A-Z\s]/, ""), row.css("span.Assist").text.gsub(/[a-z\s]|[A-Z\s]/, "")]
     end
+    kdaArray
   end
+
+
 end
