c1a48b4f18da2a1a7830d43657899745a6b1b7b8
Cristian Deleon <libaration@gmail.com>
2020-08-14 23:50:30 -0400

    fixed unnecessary double quotes changed variable names to be more clear. Called upon Scrape method in getdata instead of repeating code

--- lib/LeagueInfo/matches.rb
+++ lib/LeagueInfo/matches.rb
@@ -13,7 +13,7 @@ class LeagueInfo::Matches
   end
 
   def champsPlayed
-    champsPlayedArray = Array.new.tap do |array|
+    champsPlayedArray = [].tap do |array|
       @champsPlayed.each do |champid|
         array << champid.values.first.to_s
       end
@@ -28,17 +28,17 @@ class LeagueInfo::Matches
     matchIds = []
     champsPlayed = []
     matchData = []
-    matches = LeagueInfo::Getdata.get("https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/#{name.accountId}?endIndex=10&api_key=#{LeagueInfo::Getdata.APIKEY}")[:matches]
+    matchesjson = LeagueInfo::Getdata.get("https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/#{name.accountId}?endIndex=10&api_key=#{LeagueInfo::Getdata.APIKEY}")[:matches]
     puts ' Iterating through match data: '.green
-    bar = TTY::ProgressBar.new(" [:bar]".green , total: matches.count, width: 77) # total is the count of matches progress is advanced inside loop below by each iteration
-    matches.each do |k|
-      k.each { |key, value| matchIds << {key => value} if key == :gameId}
-      k.each { |key, value| champsPlayed << {key => value} if key == :champion}
+    bar = TTY::ProgressBar.new(' [:bar]'.green , total: matchesjson.count, width: 77) # total is the count of matches progress is advanced inside loop below by each iteration
+    matchesjson.each do |match|
+      match.each { |key, value| matchIds << {key => value} if key == :gameId}
+      match.each { |key, value| champsPlayed << {key => value} if key == :champion}
     end
     matchIds.each_with_index do |_ , i| # this loop iterates through each gameId and sets it to the currentGameId
-      currentGameId = matchIds[i].values.join()
+      currentGameId = matchIds[i].values.join
       matchHistory = LeagueInfo::Getdata.get("https://na1.api.riotgames.com/lol/match/v4/matches/#{currentGameId}?api_key=#{LeagueInfo::Getdata.APIKEY}")[:teams] # pulls match data depending on current gameId
-      return 'No matches!' if matchHistory == nil # temporary solution for matches too old to lookup
+      return 'No matches!' if matchHistory.nil? # temporary solution for matches too old to lookup
       matchData << matchHistory # push match data from current iteration into an array
         bar.advance(1)
     end
@@ -49,40 +49,39 @@ class LeagueInfo::Matches
     all.select { |obj| obj.owner == owner}
   end
 
-  def self.have_matches?(owner)
-     self.all.any?{ |obj| obj.owner == (owner) }
+  def self.have_matches?(owner) 
+    all.any?{ |obj| obj.owner == owner }
   end
 
   def self.createMatches(matches, champsPlayed)
     owner = LeagueInfo::Users.current
     matches.each_with_index do |game, _| # This gives all games in an array each game containing 2 hashes one for the winning team one for losing team so iterate through each game with index
       # index is not used here but left as a reminder you use index to go a level deeper into the hash
-      self.new(game, owner, champsPlayed) unless self.all.any?{ |obj| obj.teams.include? (game) }
+      new(game, owner, champsPlayed) unless all.any?{ |obj| obj.teams.include? game }
     end
   end
 
   def self.most_played
     champFrequency = {}
-    self.all_by_name(LeagueInfo::Users.current).each do |match|
+    all_by_name(LeagueInfo::Users.current).each do |match|
       match.champsPlayed
       match.champsPlayed.collect {|champid| champFrequency[champid.to_sym] = match.champsPlayed.count(champid) }
     end
     if LeagueInfo::Champions.valid?(champFrequency.key(champFrequency.values.max).to_s) == true
-      puts "          Most played champion: ".blue + "#{LeagueInfo::Champions.find_by_id(champFrequency.key(champFrequency.values.max).to_s).name}"
+      puts '          Most played champion: '.blue + LeagueInfo::Champions.find_by_id(champFrequency.key(champFrequency.values.max).to_s).name.to_s
     else
-      puts "          Most played champion: Not in database (New Champion)"
+      puts '          Most played champion: Not in database (New Champion)'
     end
   end
 
   def self.scrape_kda(name)
     puts ' Scraping external data'.green
     name = URI.escape name
-    kdaArray = Array.new
-    url = "https://na.op.gg/summoner/userName=#{name}"
-    doc = Nokogiri::HTML(open(url))
-    doc.css("div.GameItemWrap").each do |row|
-      kdaArray << [row.css("span.Kill").text.gsub(/[a-z\s]|[A-Z\s]/, ""), row.css("span.Death").text.gsub(/[a-z\s]|[A-Z\s]/, ""), row.css("span.Assist").text.gsub(/[a-z\s]|[A-Z\s]/, "")]
+    doc = LeagueInfo::Getdata.scrapeData("https://na.op.gg/summoner/userName=#{name}")
+    kdaArray = Array.new.tap do |array|
+      doc.css('div.GameItemWrap').each do |row|
+        array << [row.css('span.Kill').text.gsub(/[a-z\s]|[A-Z\s]/, ''), row.css('span.Death').text.gsub(/[a-z\s]|[A-Z\s]/, ''), row.css('span.Assist').text.gsub(/[a-z\s]|[A-Z\s]/, '')]
+      end
     end
-    kdaArray
   end
 end
